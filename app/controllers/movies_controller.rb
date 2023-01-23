5bcf9152f4413b18d47debf65db8ec03f7fed6e0
asma sadqi <asma.sadqi@gmail.com>
2023-01-23 13:02:07 +0100

    rails active record search

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,16 @@
+class MoviesController < ApplicationController
+
+  def index     
+    if params[:query].present?
+      sql_query = <<~SQL
+      movies.title @@ :query
+      OR movies.synopsis @@ :query
+      OR directors.first_name @@ :query
+      OR directors.last_name @@ :query
+    SQL
+    @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
+  end
+end
